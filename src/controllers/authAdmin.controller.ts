045031af4b0d87c3c1c7fb64615d21c79fcd01a3
Uriel <urielrre@gmail.com>
2025-06-01 00:56:46 -0400

    back-init

---
+++ src/controllers/authAdmin.controller.ts
@@ -0,0 +1,11 @@
+import {Request, Response} from 'express';
+import prisma from '../db/prisma';
+
+export class authAdminController {
+    static async logIn () {
+        
+    };
+    static logOut () {
+
+    };
+}
